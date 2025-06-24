892f19957aa0bbfd71bc6793f1de2a70227f163f
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-06-24 14:58:03 +0800

    small_changes

--- utilities.py
+++ utilities.py
@@ -6,7 +6,10 @@ def check_collision(pos1, pos2):
 def load_high_score(filename="highscore.txt"):
     if os.path.exists(filename):
         with open(filename, 'r') as f:
-            return int(f.read().strip())
+            try:
+                return int(f.read().strip())
+            except:
+                return 0
     return 0
 
 def save_high_score(score, filename="highscore.txt"):
