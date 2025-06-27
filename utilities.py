1a80d99d6772a844ba4510a494d670e1fc5a0703
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-06-27 19:46:10 +0800

    refactor_adding_per_mode_leaderboard_and_player_scores

--- utilities.py
+++ utilities.py
@@ -1,17 +1,39 @@
 import os
+import json
 
 def check_collision(pos1, pos2):
     return pos1 == pos2
 
-def load_high_score(filename="highscore.txt"):
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
-            try:
-                return int(f.read().strip())
-            except:
-                return 0
+HIGHSCORE_FILE = "highscore.json"
+MAX_ENTRIES = 10
+
+def load_all_highscores():
+    if not os.path.exists(HIGHSCORE_FILE):
+        return {"normal": [], "blitz": [], "inverted": []}
+    with open(HIGHSCORE_FILE, 'r') as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return {"normal": [], "blitz": [], "inverted": []}
+
+def save_all_highscores(data):
+    with open(HIGHSCORE_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def add_score(mode, name, score):
+    data = load_all_highscores()
+    if mode not in data:
+        data[mode] = []
+    data[mode].append({"name": name, "score": score})
+    data[mode] = sorted(data[mode], key=lambda x: x['score'], reverse=True)[:MAX_ENTRIES]
+    save_all_highscores(data)
+
+def get_high_score(mode):
+    data = load_all_highscores()
+    if mode in data and data[mode]:
+        return data[mode][0]["score"]
     return 0
 
-def save_high_score(score, filename="highscore.txt"):
-    with open(filename, 'w') as f:
-        f.write(str(score))
+def get_leaderboard(mode):
+    data = load_all_highscores()
+    return data.get(mode, [])
